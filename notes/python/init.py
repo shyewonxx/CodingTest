a58241be1ad3955ba94ab878f3b0fdb0ff1cf3c8
shyewonxx <shwzzang01@gmail.com>
2025-04-25 23:32:19 +0900

    .

--- notes/python/init.py
+++ notes/python/init.py
@@ -3,10 +3,16 @@
 a = 4
 b = 2
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
 
 print(a % b) #나머지
-print(a//b) #몫
+print(a // b) #몫
+
+print(a ** b) #거듭제곱
+print(a ** (1/b)) #제곱근
+
+result = a + b
+print('result : {}'.format(result))
